ef87c28bc07e710c0e87b66765b38664adf9fb31
Alvin Tandiardi <alvintandiardi@gmail.com>
2021-11-29 20:41:42 +0700

    Done Layouting Sign In Page
    - Create Sign In Page
    - Next: Email and Password Verification Logic

---
+++ app/src/main/java/com/alodokter/learncollab/SignInActivity.kt
@@ -0,0 +1,18 @@
+package com.alodokter.learncollab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alodokter.learncollab.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
